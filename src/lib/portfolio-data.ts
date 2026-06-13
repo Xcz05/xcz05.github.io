3cf5006565efcb83570ed0497d58ad41db65d907
Farezzii173 <raffifahrezi95@gmail.com>
2026-06-13 22:16:46 +0700

    Rename bisa ai logo

--- src/lib/portfolio-data.ts
+++ src/lib/portfolio-data.ts
@@ -245,7 +245,7 @@ export const certificates = [
     name: "UI/UX Designer & Researcher (MSIB Batch 6) at PT. BISA ARTIFISIAL INDONESIA (BISA.AI Academy)",
     issuer: "PT. BISA ARTIFISIAL INDONESIA & KEMENDIKBUDRISTEK",
     year: "2024",
-    logo: "/Sertifmsib/logobisa.png",
+    logo: "/Sertifmsib/Logobisa.png",
     credentialId: "No. 8922993", 
     verified: true,
     images: [
